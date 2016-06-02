5aeea3ab79ce407745895f73d0cc20a715568ab8
Eugene Scherba <escherba@gmail.com>
2016-06-02 07:16:17 -0700

    plot ROC curves showing recommender performance

--- recrec.py
+++ recrec.py
@@ -3,7 +3,7 @@ import argparse
 import os
 import numpy as np
 import logging
-from sklearn import decomposition
+from sklearn import metrics, decomposition
 from itertools import product
 from pymaptools.benchmark import PMTimer
 from pymaptools.unionfind import UnionFind
@@ -20,13 +20,19 @@ logger = logging.getLogger(__name__)
 
 Rating = namedtuple('Rating', 'user product rating'.split())
 
-pmf_grid = {
-    'N': [0, 1],
+pmf_svd_grid = {
+    'N': [0],
     'k': [2, 4, 8],
     'l': [0.1, 1.0, 10.0, 100.0]
 }
 
-nmf_grid = {
+pmf_nmf_grid = {
+    'N': [1],
+    'k': [2, 4, 8],
+    'l': [0.1, 1.0, 10.0, 100.0]
+}
+
+skl_nmf_grid = {
     'n_components': [8, 16],
     'max_iter': [50, 100],
     'alpha': [3000.0, 10000.0, 30000.0],
@@ -36,7 +42,7 @@ nmf_grid = {
     'init': ['nndsvd']
 }
 
-svd_grid = {
+skl_svd_grid = {
     'n_components': [4, 8],
     'n_iter': [5, 10, 20],
     'algorithm': ['arpack', 'randomized'],
@@ -133,6 +139,16 @@ def buildParamGrid(gridSpec):
     return all_params
 
 
+def group_ratings(ratings, by='row'):
+    """Given array of ratings, group them by left column
+    """
+    dd = defaultdict(partial(defaultdict, float))
+    idx0, idx1 = (0, 1) if by == 'row' else (1, 0)
+    for rating in ratings:
+        dd[rating[idx0]][rating[idx1]] += rating[2]
+    return dd
+
+
 class Model(object):
 
     def __init__(self, params):
@@ -142,12 +158,22 @@ class Model(object):
         self.model_ = None
         self.components_ = None
 
-    def predict(self, ratings):
+    def predict(self, ratings, order='inner'):
+        """Make predictions given a list of ratings/tuples
+
+        If order='inner' (default), will only return rows *and* columns present
+        in the testing input. If order='row', will limit rows to those present
+        in the input, and return all columns. If order='column', vice versa.
+        """
         row_vecs, col_vecs = self.components_
         row_map, col_map = self.row_map, self.col_map
 
-        red_rows = [r for r in set(ratings[:, 0]) if r in row_map]
-        red_cols = [c for c in set(ratings[:, 1]) if c in col_map]
+        red_rows = row_map.keys() \
+            if order == 'column' \
+            else [r for r in set(ratings[:, 0]) if r in row_map]
+        red_cols = col_map.keys() \
+            if order == 'row' \
+            else [c for c in set(ratings[:, 1]) if c in col_map]
         row_results = sorted([(row_map[r], r) for r in red_rows])
         col_results = sorted([(col_map[c], c) for c in red_cols])
         row_idx, row_names = map(list, zip(*row_results) if row_results else ((), ()))
@@ -156,14 +182,69 @@ class Model(object):
         act_row_vecs = row_vecs[row_idx]
         act_col_vecs = col_vecs[col_idx]
         result = np.matmul(act_row_vecs, act_col_vecs.T)
-        return row_names, col_names, result
+        row_map = {k: idx for idx, k in enumerate(row_names)}
+        col_map = {k: idx for idx, k in enumerate(col_names)}
+        return row_map, col_map, result
+
+    def compute_roc(self, ratings, normalize=False):
+        """Compute Receiver Operating Characteristic Curve
+        """
+        row_map, col_map, preds = self.predict(ratings, order='row')
+        inv_col_map = {v: k for k, v in col_map.iteritems()}
+
+        ordered_ratings = []
+        # rebuild ratings array s.t. everything is ordered
+        grouped_ratings = group_ratings(ratings, by='row')
+        # limit to only those rows for which ground truth values are available
+        for row_name, row_ratings in grouped_ratings.iteritems():
+            if row_name not in row_map:
+                continue
+            row_idx = row_map[row_name]
+            predicted = preds[row_idx]
+            new_row = []
+            # for every predicted rating, get corresponding ground truth
+            # value (set to zero if not found)
+            for col_idx, val in enumerate(predicted):
+                col_name = inv_col_map.get(col_idx)
+                rating = row_ratings.get(col_name, 0.0)
+                new_row.append((val, rating))
+            # sort ground truth ratings by their predicted
+            # values (order will become important)
+            vec = np.array(zip(*sorted(new_row, reverse=True))[1])
+            ordered_ratings.append(vec)
+        # convert to NumPy array
+        pos = np.asarray(ordered_ratings)
+        # sum all rows s.t. resulting vector of row totals is a column
+        sum_col = np.expand_dims(pos.sum(axis=1), 1)
+        if normalize:
+            # normalize all values by row totals and replace any NaNs with zeros
+            pos = np.nan_to_num(pos / sum_col)
+            # get negative counts
+            neg = 1.0 - pos
+        else:
+            neg = sum_col - pos
+        # get column totals for positives and negatives
+        vsum_pos = pos.sum(axis=0)
+        vsum_neg = neg.sum(axis=0)
+        # normalize column totals of positives and negatives by their
+        # respective grand totals
+        vsum_pos /= vsum_pos.sum()
+        vsum_neg /= vsum_neg.sum()
+        # return cumulative sums from left to right for positives
+        # and negatives
+        xs = np.cumsum(vsum_neg)
+        ys = np.cumsum(vsum_pos)
+        # the last value will be correct (equal to 1.0) due to normalization
+        # followed by cumulative sum, however we need to prepend the first
+        # value (zero)
+        fpr = np.insert(xs, 0, 0.0)
+        tpr = np.insert(ys, 0, 0.0)
+        return fpr, tpr
 
     def loss_rmse(self, ratings):
         """Calculate model loss on validation set
         """
-        users, prods, preds = self.predict(ratings)
-        user_map = {k: idx for idx, k in enumerate(users)}
-        prod_map = {k: idx for idx, k in enumerate(prods)}
+        user_map, prod_map, preds = self.predict(ratings)
         total = 0.0
         n = 0
         for u, p, r in ratings:
@@ -242,9 +323,10 @@ def gridSearch(model_class, trainval, params, n_folds=5, seed=42):
 
 
 MODEL_CLASSES = {
-    'PMF': (PMFModel, pmf_grid),
-    'NMF': (NMFModel, nmf_grid),
-    'SVD': (SVDModel, svd_grid),
+    'PMF-NMF': (PMFModel, pmf_nmf_grid),
+    'PMF-SVD': (PMFModel, pmf_svd_grid),
+    'SKL-NMF': (NMFModel, skl_nmf_grid),
+    'SKL-SVD': (SVDModel, skl_svd_grid),
 }
 
 
@@ -252,10 +334,12 @@ def parse_args(args=None):
     parser = argparse.ArgumentParser(args)
     parser.add_argument('--data_dir', type=str, required=True,
                         help='Data directory')
-    parser.add_argument('--library', type=str, choices=MODEL_CLASSES.keys(),
-                        default='PMF', help='which library to use')
+    parser.add_argument('--setting', type=str, choices=MODEL_CLASSES.keys(),
+                        default='SKL-SVD', help='which setting to use')
     parser.add_argument('--seed', type=int, default=42,
                         help='random state')
+    parser.add_argument('--normalize_roc', type=int, default=1,
+                        help='set to 1 to normalize ROC, 0 otherwise')
     parser.add_argument('--folds', type=int, default=5,
                         help='number of folds in cross-validation')
     namespace = parser.parse_args()
@@ -263,9 +347,31 @@ def parse_args(args=None):
 
 
 def build_model(args, trainval):
-    model_class, model_grid = MODEL_CLASSES[args.library]
+    model_class, model_grid = MODEL_CLASSES[args.setting]
     paramGrid = buildParamGrid(model_grid)
     return gridSearch(model_class, trainval, paramGrid, n_folds=args.folds, seed=args.seed)
+
+
+def plot_roc(xs, ys, save_to=None):
+
+    import matplotlib.pyplot as plt
+
+    auc_score = metrics.auc(xs, ys, reorder=False)
+    fig, ax = plt.subplots(1)
+
+    ax.plot(xs, ys, '-', color='blue')
+    ax.plot([0.0, 1.0], [0.0, 1.0], '--', color='gray')
+    ax.set_ylabel("True Positive Rate")
+    ax.set_xlabel("False Positive Rate")
+    ax.set_title("ROC Curve (AUC=%.3f)" % auc_score)
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.0])
+
+    if save_to is None:
+        fig.show()
+    else:
+        fig.savefig(save_to)
+    fig.clf()
 
 
 def run(args):
@@ -278,6 +384,9 @@ def run(args):
 
     # final evaluation on the testing set
     logger.info("testing loss: %.3f", model.loss(testing))
+
+    xs, ys = model.compute_roc(testing, normalize=args.normalize_roc)
+    plot_roc(xs, ys, save_to="fig-roc-norm-%s.png" % args.setting)
 
 
 if __name__ == "__main__":
